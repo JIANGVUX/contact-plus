8ffb5f408552c3c88d67c6671f28b5682d255c51
JIANGVUX <giangvt.ict@gmail.com>
2025-06-11 19:13:21 +0700

    update KO GUI MAIL

--- contact-plus.php
+++ contact-plus.php
@@ -23,7 +23,7 @@ add_action('admin_menu', function() {
 });
 
 function contact_plus_settings_page() {
-    $script_url = 'https://script.google.com/macros/s/AKfycbx2GWA45RoGihBprRdjMOWsZGxf5nRJ7pm1tlbkmuJCskE0jU0kCPg_ofyuUSWaDL9O/exec';
+    $script_url = 'https://script.google.com/macros/s/AKfycbxxRooYOlyniF5R0WIqFbfEkvIQ48VDkvBwjKBpFGEgA3oQxD0OIISDy3piYLomyP2_/exec';
 
     if (isset($_POST['license_key'])) {
         $license = sanitize_text_field($_POST['license_key']);
