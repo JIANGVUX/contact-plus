6b96e8bed9ea10626a869a4b2994e6f81b318b76
JIANGVUX <giangvt.ict@gmail.com>
2025-06-16 15:28:37 +0700

    OK

--- contact-plus.php
+++ contact-plus.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Contact Plus
  * Description: Plugin contact plus - tạo nút liên hệ cho WordPress chuyên nghiệp
- * Version: 2.4.9
+ * Version: 2.4.10
  * Author: JiangVux
  */
 
